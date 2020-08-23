5b8cd033030d8446626d78fbb5eb086fe31d1f1f
Arda İpek <arda.ipek@primeapps.io>
2020-08-23 21:13:25 +0300

    latest rate endpoint added

--- server.js
+++ server.js
@@ -1,44 +1,59 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const fetch = require("node-fetch");
 
-const API_URL = "https://api.exchangeratesapi.io/latest?base="
-
+const API_URL = "https://api.exchangeratesapi.io/latest?base=";
 
 // Swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.get('/', function (req, res) {
-    res.send("Hi");
-})
+app.get("/", function (req, res) {
+  res.send("Hi");
+});
 
 // return daily rates
 app.get("/api/daily_rates/:type?", function (request, response) {
-    var type = (request.params.type) != null ? (request.params.type) : "USD";
-    var convertedToUppercase = type.toUpperCase() === type ? type : type.toUpperCase();
+  var type = request.params.type != null ? request.params.type : "USD";
+  var convertedToUppercase =
+    type.toUpperCase() === type ? type : type.toUpperCase();
 
-    fetch(API_URL+ convertedToUppercase )
-    .then((res) => { 
-      status = res.status; 
-      return res.json()
+  fetch(API_URL + convertedToUppercase)
+    .then((res) => {
+      status = res.status;
+      return res.json();
     })
     .then((jsonData) => {
-      let returnArray = Object.entries(jsonData)
+      let returnArray = Object.entries(jsonData);
       response.send(returnArray);
     })
     .catch((err) => {
       console.error(err);
     });
-})
-  
+});
 
+// include altın
+app.get("/api/latest_rates", function (request, response) {
+  var apiURl = "https://finans.truncgil.com/today.json";
+  fetch(apiURl)
+    .then((res) => {
+      status = res.status;
+      return res.json();
+    })
+    .then((jsonData) => {
+      let returnArray = Object.entries(jsonData);
+      response.send(returnArray);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is running ", this.address().port);
-  });
+app.listen(process.env.PORT || port, function () {
+  console.log("Server is running ", this.address().port);
+});
