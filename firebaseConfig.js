f82d4d28e44642f972696edb600af357df1d780c
Arda İpek <arda.ipek@primeapps.io>
2020-08-30 13:48:41 +0300

    fixed

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAiiZqUkzxzA4mLcD4seF7BPnzSA-bE8TQ",
   authDomain: "exchange-reminder.firebaseapp.com",
   databaseURL: "https://exchange-reminder.firebaseio.com",
-  projectId: "exchange-reminder",
+  projectId: "null",
   storageBucket: "exchange-reminder.appspot.com",
   messagingSenderId: "1077017905645",
   appId: "1:1077017905645:web:01dcc1800aa7c80590ffe4"
